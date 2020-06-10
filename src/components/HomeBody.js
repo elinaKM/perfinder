071b0e95d5571f15eab08a47cd73fd0bf52600a4
unknown <elina666@gmail.com>
2020-06-10 12:39:16 +0300

    Adding Basic HomePage

---
+++ src/components/HomeBody.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HomeBody = () => (
+    <Wrapper>
+        Here will be body
+    </Wrapper>
+)
+
+const Wrapper = styled.div`
+`
+export default HomeBody
