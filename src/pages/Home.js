c89176c6d2e0fba0de7774eff81f39748b756dfe
unknown <elina666@gmail.com>
2020-06-18 18:16:29 +0300

    remove unrequired imports

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import Header from '../components/Header'
 import Footer from '../components/Footer'
-import HomeBody from '../components/HomeBody'
 import Wellcome from '../components/Wellcome'
 
 const Home = () => (
