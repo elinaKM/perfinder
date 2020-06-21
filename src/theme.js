4dc85504fcf63228e99b802b2c82dbaae88074dc
unknown <elina666@gmail.com>
2020-06-21 11:54:22 +0300

    theme file added

---
+++ src/theme.js
@@ -0,0 +1,29 @@
+const theme = {
+  colors: {
+    primary: "#87ABE0",
+    secondary: "#F8DEEB",
+    text: "#555555",
+  },
+  screenWidthBreakPoints: {
+    mobile: "420px",
+  },
+  borderRadius: {
+    button: "4px",
+  },
+  fontSizes: {
+    smallIconText: "0.5rem", // 8px
+    sponsoredLabel: "0.75rem", // 12px
+    smallDescription: "0.8125rem", // 13px
+    description: "0.875rem", // 14px
+    subText: "0.9365rem", // 15px
+    text: "1rem", // 16px
+    smallTitle: "1.125rem", // 18px
+    subTitle: "1.25rem", // 20px
+    title: "1.5rem", // 24px
+    listingTitle: "1.75rem", //28px
+    bigTitle: "2rem", // 32px
+    megaTitle: "2.25rem", // 36px
+  },
+};
+
+export default theme;
