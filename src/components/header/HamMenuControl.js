abc6a4fa9e80c906fcd47d2750feacb390bc5f95
unknown <elina666@gmail.com>
2020-06-23 10:54:53 +0300

    adding HamMenuContoll to Header

--- src/components/header/HamMenuControl.js
+++ src/components/header/HamMenuControl.js
@@ -2,13 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 import menuIcon from '../../images/icons/ham-menu.svg'
 
-const HamMenuControl = () => (
-    <Wrapper>
-        <img src={menuIcon} alt="mobile menu" />
-    </Wrapper>
+const HamMenuControl = ({ onClick }) => (
+  <Wrapper onClick={onClick}>
+    <img src={menuIcon} alt="navigation menu" />
+  </Wrapper>
 )
 
 const Wrapper = styled.div`
+  width: 20px;
+  margin-left: 15px;
 `
 
 export default HamMenuControl
