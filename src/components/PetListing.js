d2a300b4c49f29b8eac8f55aacdc29eab34efc58
unknown <elina666@gmail.com>
2020-06-23 13:34:39 +0300

    pet passed as props

--- src/components/PetListing.js
+++ src/components/PetListing.js
@@ -1,32 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import PetCard from './PetCard'
-import catImage from '../images/089.jpg'
 
-const pets = [
-    {   
-        name: "Jackie",
-        img: catImage,
-        id: "1234"
-    },
-    {   
-        name: "Pensil",
-        img: catImage,
-        id: "12345"
-    },
-    {   
-        name: "Bob",
-        img: catImage,
-        id: "123456"
-    },
-    {   
-        name: "John Snow",
-        img: catImage,
-        id: "123456"
-    },
-];
-
-const PetListing = () => (
+const PetListing = ({pets}) => (
     <Wrapper>
         {
             pets.map((pet) =>
