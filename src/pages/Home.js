966f3916af535507e3c65acd617a10eb08edd19b
unknown <elina666@gmail.com>
2020-06-23 11:00:49 +0300

    adding side menu and backdrop

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import Footer from '../components/Footer'
 import Wellcome from '../components/Wellcome'
+import NavMenu from '../components/sideMenu/SideDrawerMenu'
+import Backdrop from './../components/common/Backdrop'
 
-const Home = () => (
-    <Wrapper>
-        <Wellcome/>
-        <Footer/>
-    </Wrapper>
-)
+const Home = () => {
+    const [showMenu, setShowMenu] = useState(false);
+    
+    return (
+        <Wrapper>
+            <NavMenu open={showMenu} onCloseMenu={() => setShowMenu(false)}/>
+            { showMenu && <Backdrop onClick={() => setShowMenu(false)}/>}
+            <Wellcome setShowMenu={setShowMenu}/>
+            <Footer/>
+        </Wrapper>
+    )
+}
 
 const Wrapper = styled.div`
     height: 100vh;
@@ -16,8 +24,6 @@ const Wrapper = styled.div`
     flex-direction: column;
     justify-content: space-between;
     font-family: Arial, Helvetica, sans-serif;
-    font-size: 14px;
-    font-weight: bold;
 `
 
 export default Home
