390f5f38322aac0b751da07160a2ccd0b810cb0c
unknown <elina666@gmail.com>
2020-07-01 10:58:12 +0300

    Fixing router  - header and footer are now static

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router } from "@reach/router";
 import * as serviceWorker from "./serviceWorker";
-import Home from "./pages/Home";
 import { ThemeProvider } from "styled-components";
 import theme from "./theme";
-import PetPage from "./pages/PetPage";
-import NotFound from "./pages/NotFound";
+import App from "./App";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <React.StrictMode>
-      <Router>
-        <Home path="petfinder" />
-        <PetPage path="petfinder/pets/:petId" />
-        <NotFound default />
-      </Router>
+      <App />
     </React.StrictMode>
   </ThemeProvider>,
   document.getElementById("root")
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,8 @@
-import React, {useState} from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import Footer from '../components/Footer'
 import Wellcome from '../components/Wellcome'
 import PetListingPreview from '../components/PetListingPreview'
 import catImage from '../images/089.jpg'
-import SideDrawerMenu from '../components/sideMenu/SideDrawerMenu'
-import Backdrop from './../components/common/Backdrop'
-import '../index.css'
 
 const pets = [
     {
@@ -26,31 +22,16 @@ const pets = [
     },
 ];
 
-
-const Home = () => {
-    const [isMenuVisible, setMenuVisible] = useState(false);
-    
-    const sideDrawerCloseHandler = () => setMenuVisible(false);
-
-    return (
-        <Wrapper>
-            <SideDrawerMenu isOpen={isMenuVisible} onCloseMenu={sideDrawerCloseHandler}/>
-            {isMenuVisible && <Backdrop clickHandler ={sideDrawerCloseHandler}/>}
-            <Wellcome setMenuVisible={setMenuVisible}/>
-            <PetListingPreview pets={pets}/>
-            <Footer/>
-        </Wrapper>
-    )
-}
+const Home = () => (
+    <Wrapper>
+        <Wellcome />
+        <PetListingPreview pets={pets} />
+    </Wrapper>
+)
 
 const Wrapper = styled.div`
-    height: 100vh;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
-    font-size: 14px;
-    font-family: Nexa, Arial, Helvetica, sans-serif;
-    color: ${props => props.theme.colors.text};
 `
 
 export default Home
