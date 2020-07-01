3d92dec4d7173adb704f4770160d8c1dfb55577b
unknown <elina666@gmail.com>
2020-07-01 10:55:26 +0300

    Making transparent and more styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 import HamMenuControl from './header/HamMenuControl'
+import { Link } from '@reach/router'
+
 
 const Header = ({ setMenuVisible }) => (
     <Wrapper>
-        <LogoLink href="#">
+        <LogoLink to="petfinder">
             BestFriend.
-            </LogoLink>
+        </LogoLink>
         <HamMenuControl clickHandler={setMenuVisible}/>
     </Wrapper>
 )
@@ -15,16 +17,23 @@ const Wrapper = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    height: 70px;
-    padding: 0px 30px;
-    align-items: center;
-    margin: 0px;
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    z-index: 99;
+    background-color: rgba(250,232,242, 0.5);
+    padding-bottom: 8px;
+    box-shadow: 0 8px 6px -6px ${props => props.theme.colors.border};
 `
 
-const LogoLink = styled.a`
+const LogoLink = styled(Link)`
     text-decoration: none;
     color: ${props => props.theme.colors.primary};
     font-size: ${props => props.theme.fontSizes.title};
+    font-weight: bold;
+    margin-top: 8px;
+    margin-left: 30px;
 `
 
 export default Header
