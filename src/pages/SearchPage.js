9e85c7a8f2bb02b103f461d176bb05beac13413d
unknown <elina666@gmail.com>
2020-07-09 15:00:29 +0300

    adding selectors update earch parameters

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 import PetListing from './../components/PetListing'
-import petsDB from './../petsDB'
+import LabeledSelect from './../components/common/LabeledSelect'
+import { petsDB, typeOpt, dogBreedsOpt, ageOpt, sizeOpt, genderOpt } from './../petsDB'
 
 const SearchPage = (props) => {
+    const [type, setType] = useState("Animal");
+    const [breeds, setBreeds] = useState([]);
+    const [age, setAge] = useState([]);
+    const [size, setSize] = useState([]);
+    const [gender, setGender] = useState();
+
+    const getStringValues = (value) => {
+        return value == null ? [] : value.map((item) => item.value);
+    }
+
     return(
         <Wrapper>
             <SidePanel>
                 <Selectors>
-                    field <br/>
-                    field <br/>
-                    field <br/>
-                    field <br/>
+                    <LabeledSelect value={type} label="TYPE" options={typeOpt} onChange={(evt) => setType(evt.value)}/>
+                    <LabeledSelect  isMulti={true} label="BREEDS" options={dogBreedsOpt} value={breeds} onChange={(evt) => setBreeds(getStringValues(evt))}/>
+                    <LabeledSelect  isMulti={true} label="AGE" options={ageOpt} value={age} onChange={(evt) => setAge(getStringValues(evt))}/>
+                    <LabeledSelect  isMulti={true} label="SIZE" options={sizeOpt} value={size} onChange={(evt) => setSize(getStringValues(evt))}/>
+                    <LabeledSelect  value={gender} label="GENDER" options={genderOpt} onChange={(evt) => setGender(evt.value)}/>
                 </Selectors>
             </SidePanel>
             <ListingContainer>
+                <Title>
+                    Available {type}s for Adoption:
+                </Title>
                 <PetListing pets={petsDB}/>
             </ListingContainer>
         </Wrapper>
@@ -23,10 +38,11 @@ const SearchPage = (props) => {
 
 const Wrapper = styled.div`
     display: flex;
+    margin-top: 80px;
 `
 
 const SidePanel = styled.div`
-    width: 200px;
+    margin-right: 50px;
 `
 
 const Selectors = styled.div`
@@ -37,5 +53,12 @@ const Selectors = styled.div`
 `
 
 const ListingContainer = styled.div`
+
 `
+
+const Title = styled.div`
+    font-size: ${props => props.theme.fontSizes.title};
+    margin-bottom: 20px;
+`
+
 export default SearchPage
