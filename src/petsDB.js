472435d316f721b0ea55633b8ee72707409b25c5
unknown <elina666@gmail.com>
2020-07-09 14:54:07 +0300

    addning more data

--- src/petsDB.js
+++ src/petsDB.js
@@ -1,6 +1,6 @@
 import catImage from "./images/089.jpg";
 
-const petsDB = [
+export const petsDB = [
   {
     name: "Jackie",
     img: catImage,
@@ -16,6 +16,54 @@ const petsDB = [
     img: catImage,
     id: "123456",
   },
+  {
+    name: "Jack",
+    img: catImage,
+    id: "234",
+  },
+  {
+    name: "Luna",
+    img: catImage,
+    id: "2345",
+  },
+  {
+    name: "Yoshi",
+    img: catImage,
+    id: "23456",
+  },
 ];
 
-export default petsDB;
+export const typeOpt = [
+  { value: "Cat", label: "Cat" },
+  { value: "Dog", label: "Dog" },
+  { value: "Rabbit", label: "Rabbit" },
+];
+
+export const dogBreedsOpt = [
+  { value: "Pit Bull Terrier", label: "Pit Bull Terrier" },
+  { value: "Cane Corso", label: "Cane Corso" },
+];
+
+export const catBreedsOpt = [
+  { value: "Domestic Short Hair", label: "Domestic Short Hair" },
+  { value: "Siamese", label: "Siamese" },
+];
+
+export const ageOpt = [
+  { value: "Puppy", label: "Puppy" },
+  { value: "Young", label: "Young" },
+  { value: "Adult", label: "Adult" },
+  { value: "Senior", label: "Senior" },
+];
+
+export const sizeOpt = [
+  { value: "Small", label: "Small" },
+  { value: "Medium", label: "Medium" },
+  { value: "Large", label: "Large" },
+  { value: "Extra Large", label: "Extra Large" },
+];
+
+export const genderOpt = [
+  { value: "Male", label: "Male" },
+  { value: "Female", label: "Female" },
+];
