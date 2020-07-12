a21d2cca1c94c382f1914ba0bcba38daefca4e93
unknown <elina666@gmail.com>
2020-07-12 14:56:51 +0300

    removinh hard coded configurations

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,31 +2,12 @@ import React from 'react'
 import styled from 'styled-components'
 import Wellcome from '../components/Wellcome'
 import PetListingPreview from '../components/PetListingPreview'
-import catImage from '../images/089.jpg'
-
-const pets = [
-    {
-        name: "Jackie",
-        img: catImage,
-        id: "1"
-    },
-    {
-        name: "Pensil",
-        img: catImage,
-        id: "2"
-    },
-    {
-        name: "Bob",
-        img: catImage,
-        id: "3"
-    },
-];
-
+import { petsDB } from './../petsDB'
 
 const Home = () => (
     <Wrapper>
         <Wellcome />
-        <PetListingPreview pets={pets} />
+        <PetListingPreview pets={petsDB.slice(0,3)} />
     </Wrapper>
 )
 
